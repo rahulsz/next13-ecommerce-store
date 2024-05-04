3e8a28933efb0b3d0d98392bc83081da42115714
Rahul Shashidhar <130664248+rahulsz@users.noreply.github.com>
2024-05-04 15:32:17 +0530

    billboard updated

--- components/ui/billboard.tsx
+++ components/ui/billboard.tsx
@@ -7,6 +7,9 @@ interface BillboardProps {
 const Billboard: React.FC<BillboardProps> = ({
   data
 }) => {
+  if (!data) {
+    return null; // Or render a loading state or placeholder component
+  }
   return ( 
     <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
       <div style={{ backgroundImage: `url(${data?.imageUrl})` }} className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover">
